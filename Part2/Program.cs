79ae6829173286b8a9cc74d3e3fae5dda9ca569b
Mark Aloka <maloka@miracosta.edu>
2020-08-31 17:58:41 -0700

    Add project files.

---
+++ Part2/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: part 2
+/// @author: Mark aloka
+/// Date:  August 24, 2020
+///
+/// Problem Statement: use writeline to create a shape on the console screen.
+/// 
+/// 
+/// 
+/// Overall Plan:
+/// 1) Use writeline method to draw a shap on the screen
+/// 2) 
+/// 3) 
+/// 4) 
+/// 
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         *           ");
+            Console.WriteLine("        ***           ");
+            Console.WriteLine("       *****           ");
+            Console.WriteLine("      *******           ");
+
+        }
+    }
+}
+
